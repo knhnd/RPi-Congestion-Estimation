4ac38c60e1e5cfe70fbccf986cc62b411737962a
Ken Honda <ken.h.0802@gmail.com>
2022-01-20 00:40:14 +0900

    [UPDATE] update directory construction and add .gitignore

--- src/edge.js
+++ src/edge.js
@@ -1,4 +1,4 @@
-import WebcamControlService from './src/WebcamControlService/webcam/WebcamControlService';
+import WebcamControlService from './WebcamControlService/webcam/WebcamControlService';
 
 // RaspberryPi に接続された Webcam で写真を撮影する
 WebcamControlService.captureImage();
