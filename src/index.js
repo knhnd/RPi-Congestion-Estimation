dbb835592be84252e17fec74982d3aec91ed1be8
kenh <ken.h.0802@gmail.com>
2022-01-19 21:17:29 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+'use strict';
+const express = require('express');
+
+// create express app
+const app = express();
+
+// routing
+// test
+app.get('/',(req, res) => {
+    const test = [
+	    {message: 'It Works!'},
+    ];
+    res.json(test);
+});
+
+// 混雑度の推定
+app.get(`/congestion`, (req, res) => {
+   const result = "Work in Progress."
+   res.json(result);
+})
+
+
+const port = 3000;
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
