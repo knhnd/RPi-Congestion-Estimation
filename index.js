1a8942782b09ce4eae82b286243c51df20764626
Ken Honda <ken.h.0802@gmail.com>
2022-01-20 00:33:16 +0900

    [UPDATE] update directory construction

--- index.js
+++ index.js
@@ -6,21 +6,19 @@ const app = express();
 
 // routing
 // test
-app.get('/',(req, res) => {
-    const test = [
-	    {message: 'It Works!'},
-    ];
-    res.json(test);
+app.get('/', (req, res) => {
+  const test = [{ message: 'It Works!' }];
+  res.json(test);
 });
 
 // 混雑度の推定
 app.get(`/congestion`, (req, res) => {
-   const result = "Work in Progress."
-   res.json(result);
-})
+  const result = 'Work in Progress.';
+  res.json(result);
+});
 
-
+// Webサーバの起動
 const port = 3000;
 app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
